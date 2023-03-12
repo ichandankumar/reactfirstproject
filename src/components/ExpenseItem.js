a0f15dc84132d6890bf31dfebfa8a59b2147c97c
ichandankumar <bob@example.com>
2023-03-12 13:06:43 +0530

    expansereactnew

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
+ 
+function ExpenseItem(props) {
+  const [buttonText, setButtonText] = useState('Click me')
+ // const clickHandler = ()=> {
+ // console.log('clicked!!!')
+ // };fo
+ // const btn = document.getElementsByClassName('expense-item')
+    
+ //for (var i = 0; i < btn.length; i++) {
+   // btn[i].addEventListener('click',function (e) {      
+      //e.parentNode.parentNode.removeChild(e.parentNode);
+   // } , false);
+// }//let title= props.title;
+//const clickHandler = () => {
+  //title = "Updated!";
+ // console.log(title)
+//};
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+     
+      <button onClick={() => setButtonText('You clicked me!')}>{buttonText}</button>
+ 
+     
+    </Card>
+  );
+}
+
+export default ExpenseItem;
